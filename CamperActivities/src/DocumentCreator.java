0ec19f65a607f74989207f20bd3e272b43f0260b
cedney97 <c.edney@wustl.edu>
2022-06-12 12:04:56 -0500

    Trying to figure out document creation

---
+++ CamperActivities/src/DocumentCreator.java
@@ -0,0 +1,9 @@
+import org.apache.poi.xwpf.usermodel.XWPFDocument;  
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;  
+import org.apache.poi.xwpf.usermodel.XWPFRun;  
+
+public class DocumentCreator {
+	public static void main(String[] args) {
+		Document doc = new Document();
+	}
+}
