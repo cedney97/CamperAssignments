03da0c524064cae0111ef780c21f17ecdc50ef59
cedney97 <c.edney@wustl.edu>
2022-06-14 07:31:47 -0500

    It's not working?

--- CamperActivities/src/DocumentCreator.java
+++ CamperActivities/src/DocumentCreator.java
@@ -16,7 +16,7 @@ import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Text;
 
 public class DocumentCreator {
-	public static void main(String[] args) throws FileNotFoundException, MalformedURLException{
+	public static void main(String[] args) throws FileNotFoundException, MalformedURLException {
 		File file = new File("ActivityRoster.pdf");
 		PdfWriter pdfw = new PdfWriter(file);
 		PdfDocument pdfDoc = new PdfDocument(pdfw);
@@ -30,16 +30,45 @@ public class DocumentCreator {
 		Paragraph p = new Paragraph();
 		p.add(logo);
 		doc.add(p);
-		
+
 		Paragraph activityName = new Paragraph();
 		activityName.add("Adventure Challenge 1");
 		doc.add(activityName);
-		
+
 		Paragraph weekPeriod = new Paragraph();
 		weekPeriod.add("JW1, Period 1");
 		doc.add(weekPeriod);
-		
+
 		doc.close();
 	}
-}
 
+	public void createRoster(Activity a) throws FileNotFoundException, MalformedURLException {
+		for (Period p : a.getPeriods()) {
+			if (p != null) {
+				File file = new File(a.getName());
+				PdfWriter pdfw = new PdfWriter(file);
+				PdfDocument pdfDoc = new PdfDocument(pdfw);
+				pdfDoc.addNewPage();
+				Document doc = new Document(pdfDoc);
+				PdfDocumentInfo info = pdfDoc.getDocumentInfo();
+				info.setTitle(a.getName() + " Roster");
+
+				Paragraph pLogo = new Paragraph();
+				Image logo = new Image(ImageDataFactory.create("src/photos/FrontierCampLogo.png"));
+				logo.setHeight(logo.getImageHeight() * .5f);
+				logo.setWidth(logo.getImageWidth() * .5f);
+				pLogo.add(logo);
+				doc.add(pLogo);
+
+				Paragraph activityName = new Paragraph();
+				activityName.add(a.getName());
+				doc.add(activityName);
+
+				Paragraph weekPeriod = new Paragraph();
+				weekPeriod.add("JW1, Period " + p.getOrder());
+				doc.add(weekPeriod);
+			}
+		}
+
+	}
+}
